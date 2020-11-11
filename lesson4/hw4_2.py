57547f62a8bf5427d3352cbf3fdd400e88b26c72
Anastasia <anastasia.kurchenkova@gmail.com>
2020-11-12 02:28:12 +0300

    hw4_2modif

--- lesson4/hw4_2.py
+++ lesson4/hw4_2.py
@@ -4,7 +4,12 @@
 Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 Результат: [12, 44, 4, 10, 78, 123].
 """
+from random import randrange
 
 original_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print([original_list[i] for i in range(1, len(original_list)) if original_list[i] > original_list[i - 1]])
+print("Исходный список", original_list)
+print("Результат", [original_list[i] for i in range(1, len(original_list)) if original_list[i] > original_list[i - 1]])
 
+random_list = [randrange(-10, 10) for _ in range(13)]
+print("Рандомный список", random_list)
+print("Результат", [random_list[i] for i in range(1, len(random_list)) if random_list[i] > random_list[i - 1]])
