c980164a6024dd2cb52a1ba225262c61ae559c63
Anastasia <anastasia.kurchenkova@gmail.com>
2020-11-12 12:26:29 +0300

    hw4_1,4_2modi

--- lesson4/hw4_1.py
+++ lesson4/hw4_1.py
@@ -3,16 +3,21 @@
 В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 """
-# python3 hw4_1.py 149.5 2700 18000  данные для копирования (на mac!) и ввода в терминал
+# python3 hw4_1.py 170.5 1700 13000  данные для копирования (на mac!) и ввода в терминал
 # зп брутто: ((выработка в часах * ставка в час) + премия) + (22 + 2,9 + 5,1 + 0,2)% от первой части
 # зп нетто: ((выработка в часах * ставка в час) + премия) - 13%
 
 from sys import argv
 
-try:
-    script_name, working_hours, rate_in_hours, bonus = argv
 
-    estimation = float(working_hours) * float(rate_in_hours) + float(bonus)
+def funk_estimation(a, b, c):
+    return a * b + c
+
+
+try:
+    _, working_hours, rate_in_hours, bonus = argv
+
+    estimation = funk_estimation(float(working_hours), float(rate_in_hours), float(bonus))
     social_contributions = 22 + 2.9 + 5.1 + 0.2
 
     salary_bru = estimation + (estimation * social_contributions / 100)
--- lesson4/hw4_2.py
+++ lesson4/hw4_2.py
@@ -4,12 +4,23 @@
 Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 Результат: [12, 44, 4, 10, 78, 123].
 """
+
 from random import randrange
 
-original_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print("Исходный список", original_list)
-print("Результат", [original_list[i] for i in range(1, len(original_list)) if original_list[i] > original_list[i - 1]])
+lst_o = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print("Исходный список", lst_o)
+# for i in range(1, len(lst_o)):
+#     if lst_o[i] > lst_o[i - 1]:
+#         print([lst_o[i]])
+print("Результат range", [lst_o[i] for i in range(1, len(lst_o)) if lst_o[i] > lst_o[i - 1]])
 
-random_list = [randrange(-10, 10) for _ in range(13)]
-print("Рандомный список", random_list)
-print("Результат", [random_list[i] for i in range(1, len(random_list)) if random_list[i] > random_list[i - 1]])
+# for idx, el in enumerate(lst_o):
+#     if idx and el > lst_o[idx - 1]:
+#         print([el])
+print("Результат enume", list(el for idx, el in enumerate(lst_o) if idx and el > lst_o[idx - 1]))
+
+print("--- Рандомчик ---")
+lst_r = [randrange(-10, 10) for a in range(13)]
+print("Рандомный список №2", lst_r)
+print("Результат range", [lst_r[i] for i in range(1, len(lst_r)) if lst_r[i] > lst_r[i - 1]])
+print("Результат enume", list(el for idx, el in enumerate(lst_r) if idx and el > lst_r[idx - 1]))
