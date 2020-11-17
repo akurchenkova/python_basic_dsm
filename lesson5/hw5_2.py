13110136b1210910f8b472dc70adf9caefb33125
Anastasia <anastasia.kurchenkova@gmail.com>
2020-11-17 03:55:13 +0300

    hw5_1

---
+++ lesson5/hw5_2.py
@@ -0,0 +1,16 @@
+# 2. Создать текстовый файл (не программно), сохранить в нем несколько строк,
+# выполнить подсчет количества строк, количества слов в каждой строке.
+
+import os
+
+file2 = os.path.join(os.path.dirname(__file__), 'file2.txt')
+
+try:
+    with open(file2, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+        words_count = [(idx, len(line.split())) for idx, line in enumerate(lines, 1)]
+        print(f'Строк в файле: {len(lines)} \nКоличетство слов в каждой строке:')
+        for i, el in words_count:
+            print(f'{i}: {el}')
+except FileNotFoundError:
+    print('Файл file2.txt в рабочей директории отсутсвует')
