9f3cd41c8c7cf6312a73fda4aebee2e147211a6a
Anastasia <anastasia.kurchenkova@gmail.com>
2020-11-17 03:21:50 +0300

    hw5_1

--- lesson5/hw5_1.py
+++ lesson5/hw5_1.py
@@ -1,1 +1,13 @@
-#
+# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка.
+
+import os
+
+file1 = os.path.join(os.path.dirname(__file__), 'file1.txt')
+
+with open(file1, 'w', encoding='UTF-8') as file:
+    while True:
+        line = input('Введите данные: ')
+        if not len(line):
+            break
+        file.write(f'{line}\n')
